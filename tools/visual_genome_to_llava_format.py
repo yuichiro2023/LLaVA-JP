9ecb76af1f2fced4b9a3e1a871f70c6fe8fb2b57
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-05 21:00:00 +0900

    path修正

--- tools/visual_genome_to_llava_format.py
+++ tools/visual_genome_to_llava_format.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    dataset_dir = './'
+    dataset_dir = './dataset'
     stair_captions_dir = 'VisualGenome'
     caption_path = Path(dataset_dir, stair_captions_dir, 'question_answers.json')
     image_path = Path(dataset_dir, stair_captions_dir, 'image_data.json')
@@ -40,7 +40,7 @@ if __name__ == '__main__':
             
             stair_llava_formats.append(llava_format)
 
-    chat_ja_path = Path('input/LLaVa_visual_genome_ja', 'llava_visual_genome_ja.json')
+    chat_ja_path = Path('./dataset', 'llava_visual_genome_ja.json')
     with open(chat_ja_path, mode="w") as f:
         json.dump(stair_llava_formats, f, indent=2, ensure_ascii=False)
     
