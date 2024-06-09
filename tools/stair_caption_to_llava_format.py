f9ecc424be7ae97b71137f7cbc76babec35c2608
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-10 02:30:41 +0900

    商用利用不可の画像を除外

--- tools/stair_caption_to_llava_format.py
+++ tools/stair_caption_to_llava_format.py
@@ -15,33 +15,38 @@ if __name__ == '__main__':
         caption_data = f.read()
         caption_data_json = json.loads(caption_data)
 
+    print(caption_data_json['licenses'])
+
     for i, image in tqdm(enumerate(caption_data_json['images']), total=len(caption_data_json['images'])):
-        captions = [(annotation['id'], annotation['caption']) for annotation in caption_data_json['annotations'] if annotation['image_id'] == image['id']]
+        if image["license"] in [4, 5]:
+            captions = [(annotation['id'], annotation['caption']) for annotation in caption_data_json['annotations'] if annotation['image_id'] == image['id']]
 
-        if len(captions) == 0:
-            print("not found caption")
-            continue
+            if len(captions) == 0:
+                print("not found caption")
+                continue
 
-        for id, caption in captions:
-            llava_format = {}
-            conversations = []
+            for id, caption in captions:
+                llava_format = {}
+                conversations = []
 
-            conversation_user = {
-                'from': 'ユーザー',
-                'value': '画像について説明してください。\n<image>'
-            }
-            conversation_system = {
-                'from': 'システム',
-                'value': caption
-            }
-            conversations.append(conversation_user)
-            conversations.append(conversation_system)
+                conversation_user = {
+                    'from': 'ユーザー',
+                    'value': '画像について説明してください。\n<image>'
+                }
+                conversation_system = {
+                    'from': 'システム',
+                    'value': caption
+                }
+                conversations.append(conversation_user)
+                conversations.append(conversation_system)
 
-            llava_format['id'] = id
-            llava_format['image'] = image['file_name']
-            llava_format['conversations'] = conversations
+                llava_format['id'] = id
+                llava_format['image'] = image['file_name']
+                llava_format['conversations'] = conversations
 
-            stair_llava_formats.append(llava_format)
+                stair_llava_formats.append(llava_format)
+
+    print(len(stair_llava_formats))
 
     chat_ja_path = Path('dataset', 'LLaVA-Stair-Caption', 'llava_stair_caption.json')
     with open(chat_ja_path, mode="w") as f:
